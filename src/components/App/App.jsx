c17857cb0113306540076d1677a241630ce0dcb7
Armin Blaze <box_for_spam@mail.ru>
2019-07-16 07:00:16 +0300

    120 - Спиннер через absolute

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Spinner from 'components/Spinner/Spinner';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Spinner />
+    )
+  }
+}
+
+export default App;
