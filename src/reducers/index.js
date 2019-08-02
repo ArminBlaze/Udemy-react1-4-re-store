943bcd7526e17181b1b4ff41ec929b5e1795a7a3
Armin Blaze <box_for_spam@mail.ru>
2019-08-02 18:00:36 +0300

    136 - Обновление элементов массива Redux (моя версия)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -38,16 +38,42 @@ const reducer = (state = initialState, action) => {
     case 'BOOK_ADDED_TO_CART':
       const bookId = action.value;
       const book = state.books.find( (book) => book.id === bookId );
+      //сначала надо искать есть ли эта книга в cartItems
+      let idx;
+      const oldItem = state.cartItems.find( (item, i) => {
+        let result = false;
+        if(item.id === bookId) {
+          idx = i;
+          result = true;
+        }
+        return result
+      });
+    
+      const count = (oldItem) ? oldItem.count + 1 : 1;
+
       const newItem = {
         id: book.id,
         title: book.title,
-        count: 1,
-        total: book.price
+        count: count,
+        total: book.price * count,
       };
+
+      let cartItems = state.cartItems;
+
+
+      let newCart;
+      if(oldItem) {
+        newCart = [].concat(cartItems.slice(0, idx), newItem, cartItems.slice(idx+1, cartItems.length));
+      }
+      else {
+        newCart = [...cartItems, newItem];
+      }
+
+      console.log(newCart);
 
       return {
         ...state,
-        cartItems: [...state.cartItems, newItem],
+        cartItems: newCart,
       }
       
     
